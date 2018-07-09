393b46c0b822551063f7c5424886f6ea6149ae75
Carla Mauri <carla.mauri1@intesasanpaolo.com>
2018-07-09 15:30:24 +0200

    initial commit

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  // put setup code here
+  createCanvas(600,400);
+}
+
+function draw() {
+  // put drawing code here
+  // ellipse(40,40,40,40);
+
+  background('green');
+  fill("red"); 
+  ellipse(mouseX,mouseY,40,40);
+
+  fill("orange");
+  textSize(32);
+  text ("happy Copenhagen", 100,100);
+
+
+}

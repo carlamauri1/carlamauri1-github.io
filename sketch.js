c08cf66f87edc575a694514372caf59d14d30526
Carla Mauri <carla.mauri1@intesasanpaolo.com>
2018-07-10 11:17:06 +0200

    modify

--- sketch.js
+++ sketch.js
@@ -1,19 +1,21 @@
 function setup() {
   // put setup code here
-  createCanvas(600,400);
+  createCanvas(600,600);
 }
 
 function draw() {
   // put drawing code here
   // ellipse(40,40,40,40);
 
-  background('green');
-  fill("orange");
+  background('blue');
+  fill("white");
   ellipse(mouseX,mouseY,40,40);
 
+  fill("white");
+  rect(mouseX,mouseY,55,55);
 
 
-  fill("blue");
+  fill("white");
   textSize(32);
   text ("happy Copenhagen", 100,100);
 
